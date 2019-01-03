560004ee3adfa42bf90fba5ea0c4b54ec28eb45a
euge001 <webylon@gmail.com>
2019-01-03 17:34:20 +0700

    structure_changes

--- client/src/components/Footer/index.js
+++ client/src/components/Footer/index.js
@@ -1,36 +1,33 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
 
-import BottomNavigation from '@material-ui/core/BottomNavigation';
-import { Link } from 'react-router-dom';
-import Button from '@material-ui/core/Button';
-
+import BottomNavigation from "@material-ui/core/BottomNavigation";
+import { Link } from "react-router-dom";
+import Button from "@material-ui/core/Button";
 
 class Footer extends Component {
-    render() {
-        return (
-            <div className="bottomNav">
-                <div className="divcenter">
-                    <Link className="bottomNavLink" to="/">
-                        Home
-                 </Link>
-                    <Link className="bottomNavLink" to="/Aerial-Application-Robot">
-                        Aerial Application Robot
-                 </Link>
+  render() {
+    return (
+      <div className="bottomNav">
+        <div className="divcenter">
+          <Link className="bottomNavLink" to="/">
+            Home
+          </Link>
 
-                    <Link className="bottomNavLink" to="/Manned-Drone-System">
-                        Manned Drone System
-                 </Link>
+          <Link className="bottomNavLink" to="/Aerial-Application-Robot">
+            Aerial Application Robot
+          </Link>
 
-                    <Link className="bottomNavLink" to="/Software">
-                        Software
-                 </Link>
-                    <Link className="bottomNavLink" to="/Contacts">
-                        Contacts
-                 </Link>
-                </div>
-            </div>
-        );
-    }
+          <Link className="bottomNavLink" to="/Manned-Drone-System">
+            Manned Drone System
+          </Link>
+
+          <Link className="bottomNavLink" to="/Contacts">
+            Contacts
+          </Link>
+        </div>
+      </div>
+    );
+  }
 }
 
 export default Footer;
